20c375d6f2e9b57d9e9190df220d1436193f2142
Ingvar Vilkman <zeegin@zeegin.com>
2019-08-14 20:48:10 +0300

    Migrate to typescript

--- postcss.config.js
+++ postcss.config.js
@@ -8,6 +8,8 @@ module.exports = {
     }),
     alter({
       declarations: {
+        // exclude unsupported 1c webkit css modifier
+        // to fix non-displayed cursor on mouse over at line numbers
         'cursor': {
           task: 'remove',
           whenRegex: {
---
+++ src/languages/turbo-gherkin.contribution.ts
@@ -0,0 +1,14 @@
+interface ILangImpl {
+	conf: monaco.languages.LanguageConfiguration;
+	language: monaco.languages.IMonarchLanguage;
+}
+
+const language: monaco.languages.ILanguageExtensionPoint = { id: "turbo-gherkin" };
+
+monaco.languages.register(language);
+monaco.languages.onLanguage(language.id, () => {
+  import("./turbo-gherkin").then((module: ILangImpl) => {
+    monaco.languages.setLanguageConfiguration(language.id, module.conf);
+    monaco.languages.setMonarchTokensProvider(language.id, module.language);
+  });
+});
