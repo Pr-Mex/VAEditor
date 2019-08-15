380ed450d8b1f7bb3a78b29b174f53884c703562
Ingvar Vilkman <zeegin@zeegin.com>
2019-08-15 12:37:48 +0300

    Remove html template

---
+++ src/1c-webkit-style-patch.js
@@ -0,0 +1,11 @@
+(() => {
+  const standardScrollbarStyle = document.getElementById('1C_scrollbar_12704CA4-9C01-461B-8383-F4CD6283CB75')
+  if (standardScrollbarStyle !== null) {
+    standardScrollbarStyle.remove()
+  }
+
+  const fullscreenStyle = document.createElement('style')
+  fullscreenStyle.type = 'text/css'
+  fullscreenStyle.innerHTML = 'html, body { width: 100%; height:100%; margin: 0; padding: 0; } ::-webkit-scrollbar { display: none; }'
+  document.head.appendChild(fullscreenStyle)
+})()
