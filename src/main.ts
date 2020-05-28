6dc381ce66c3149488df2e02c2f1ddc3e8ebf440
Kandrashin Denis <mail@lintest.ru>
2020-05-28 21:56:25 +0300

    Работающий пример автоподставноки щагов

--- src/main.ts
+++ src/main.ts
@@ -29,5 +29,60 @@ window["createVanessaEditor"] = () => window["VanessaEditor"] = new VanessaEdito
 
 // tslint:disable-next-line: no-string-literal
 window["createVanessaDiffEditor"] = (original: string, modified: string, language: string) => {
-    window["VanessaEditor"] = new VanessaDiffEditor(original, modified, language);
+  window["VanessaEditor"] = new VanessaDiffEditor(original, modified, language);
 };
+
+window["VanessaKeywords"] = [
+  "Функционал",
+  "Сценарий",
+  "Контекст",
+  "Допустим",
+  "Дано",
+  "Когда",
+  "И",
+  "Не",
+  "Тогда",
+  "Затем",
+  "Если",
+  "Примеры",
+];
+
+window["VanessaStepList"] = [];
+
+window["setVanessaStepList"] = function (arg) {
+  function isKeyword(w) {
+    return window["VanessaKeywords"].some(e => e.localeCompare(w, 'ru', { sensitivity: 'base' }) == 0);
+  }
+  let steps = [];
+  JSON.parse(arg).forEach(e => {
+    let n = 0;
+    let list = e.ИмяШага.split('\n')[0].replace(/\s\s+/g, ' ').split(' ').filter((s, i) => {
+      if (!s) return false;
+      if (isKeyword(s) && n == 0) return false;
+      n = i; return s;
+    });
+
+    steps.push({
+      label: list.join(' '),
+      filterText: list.filter(s => s && s[0] != '"').join(' '),
+      documentation: e.ОписаниеШага,
+      insertText: e.ИмяШага,
+    });
+  });
+  window["VanessaStepList"] = steps;
+}
+
+window['VanessaCompletion'] = function (line, range) {
+  let result = [];
+  window["VanessaStepList"].forEach(e => {
+    result.push({
+      label: e.label,
+      kind: monaco.languages.CompletionItemKind.Function,
+      documentation: e.documentation,
+      insertText: e.insertText,
+      filterText: e.filterText,
+      range: range
+    });
+  });
+  return result;
+}
