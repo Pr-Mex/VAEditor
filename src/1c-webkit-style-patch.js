abeb5c77ca09f14d853d7361912ecb3a64285895
Kandrashin Denis <mail@lintest.ru>
2020-06-10 23:44:34 +0300

    Простейший показ ошибки внутри кода

--- src/1c-webkit-style-patch.js
+++ src/1c-webkit-style-patch.js
@@ -6,6 +6,13 @@
 
   const fullscreenStyle = document.createElement('style')
   fullscreenStyle.type = 'text/css'
-  fullscreenStyle.innerHTML = 'html, body { width: 100%; height:100%; margin: 0; padding: 0; } ::-webkit-scrollbar { display: none; } .suggest-widget{ width:800px !important; } .debug-pending-step { background: lightblue; }'
+  fullscreenStyle.innerHTML = "\
+  html, body { width: 100%; height:100%; margin: 0; padding: 0; }\
+  ::-webkit-scrollbar { display: none; }\
+  .suggest-widget{ width:800px !important; }\
+  .debug-pending-step { background: lightblue; }\
+  .vanessa-error-widget { background: #FFCCCC; padding: 0 0.5em 0 0.5em; overflow: hidden; white-space: nowrap; }\
+  .vanessa-peekview-action { background: url(data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIxNiIgaGVpZ2h0PSIxNiIgdmlld0JveD0iMyAzIDE2IDE2IiBlbmFibGUtYmFja2dyb3VuZD0ibmV3IDMgMyAxNiAxNiI+PHBvbHlnb24gZmlsbD0iIzQyNDI0MiIgcG9pbnRzPSIxMi41OTcsMTEuMDQyIDE1LjQsMTMuODQ1IDEzLjg0NCwxNS40IDExLjA0MiwxMi41OTggOC4yMzksMTUuNCA2LjY4MywxMy44NDUgOS40ODUsMTEuMDQyIDYuNjgzLDguMjM5IDguMjM4LDYuNjgzIDExLjA0Miw5LjQ4NiAxMy44NDUsNi42ODMgMTUuNCw4LjIzOSIvPjwvc3ZnPg==) center center no-repeat;}\
+  ";
   document.head.appendChild(fullscreenStyle)
 })()
