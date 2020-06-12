fffd7b0fe862bb478f7a6b83e2c7cdf6d67de3f6
Kandrashin Denis <mail@lintest.ru>
2020-06-12 17:40:11 +0300

    Удален неиспользуемый код

--- src/1c-webkit-style-patch.js
+++ src/1c-webkit-style-patch.js
@@ -11,7 +11,6 @@
   ::-webkit-scrollbar { display: none; }\
   .debug-pending-step { background: lightblue; }\
   .vanessa-error-widget { background: #FFCCCC; padding: 0 0.5em 0 0.5em; overflow: hidden; white-space: nowrap; }\
-  .vanessa-peekview-action { background: url(data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIxNiIgaGVpZ2h0PSIxNiIgdmlld0JveD0iMyAzIDE2IDE2IiBlbmFibGUtYmFja2dyb3VuZD0ibmV3IDMgMyAxNiAxNiI+PHBvbHlnb24gZmlsbD0iIzQyNDI0MiIgcG9pbnRzPSIxMi41OTcsMTEuMDQyIDE1LjQsMTMuODQ1IDEzLjg0NCwxNS40IDExLjA0MiwxMi41OTggOC4yMzksMTUuNCA2LjY4MywxMy44NDUgOS40ODUsMTEuMDQyIDYuNjgzLDguMjM5IDguMjM4LDYuNjgzIDExLjA0Miw5LjQ4NiAxMy44NDUsNi42ODMgMTUuNCw4LjIzOSIvPjwvc3ZnPg==) center center no-repeat;}\
   ";
   document.head.appendChild(fullscreenStyle)
 })()
