ed3f2e30bd21b8175de5522af6a06eff39c2b48b
Kandrashin Denis <mail@lintest.ru>
2020-06-14 01:57:07 +0300

    Пример отображения информации об ошибке

--- src/1c-webkit-style-patch.js
+++ src/1c-webkit-style-patch.js
@@ -10,7 +10,10 @@
   html, body { width: 100%; height:100%; margin: 0; padding: 0; }\
   ::-webkit-scrollbar { display: none; }\
   .debug-pending-step { background: lightblue; }\
-  .vanessa-error-widget { background: #FFCCCC; padding: 0 0.5em 0 0.5em; overflow: hidden; white-space: nowrap; }\
+  .vanessa-error-widget { background: #FFCCCC; color: #333333; padding: 0 0.5em 0 0.5em; overflow: hidden; white-space: nowrap; }\
+  .vanessa-error-widget div { position: absolute; bottom: 0; color: #333333; user-select: none; white-space: nowrap; vertical-align: sub; font-size: 90%; }\
+  .vanessa-error-widget div a { text-decoration: none; color: #333333; }\
+  .vanessa-error-widget div a:hover { color: #0000ff !important; }\
   ";
   document.head.appendChild(fullscreenStyle)
 })()
