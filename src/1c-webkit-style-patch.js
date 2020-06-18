0f96e3b872bf4b82fdd5bd09fa1b8a8639d78c43
Kandrashin Denis <mail@lintest.ru>
2020-06-18 19:34:23 +0300

    Стили статуса выполнения перенесены в CSS-файл #59

--- src/1c-webkit-style-patch.js
+++ src/1c-webkit-style-patch.js
@@ -3,17 +3,11 @@
   if (standardScrollbarStyle !== null) {
     standardScrollbarStyle.remove()
   }
-
   const fullscreenStyle = document.createElement('style')
   fullscreenStyle.type = 'text/css'
   fullscreenStyle.innerHTML = "\
   html, body { width: 100%; height:100%; margin: 0; padding: 0; }\
   ::-webkit-scrollbar { display: none; }\
-  .debug-pending-step { background: lightblue; }\
-  .vanessa-error-widget { background: #FFCCCC; color: #333333; padding: 0 0.5em 0 0.5em; user-select: none; -webkit-user-select: none; overflow: hidden; white-space: nowrap; }\
-  .vanessa-error-widget .vanessa-error-links { position: absolute; bottom: 0; color: #333333; vertical-align: sub; font-size: 85%; }\
-  .vanessa-error-widget .vanessa-error-links a { text-decoration: none; color: #333333; }\
-  .vanessa-error-widget .vanessa-error-links a:hover { color: #ff0000 !important; }\
   ";
   document.head.appendChild(fullscreenStyle)
 })()
