5c29233be60af16f9ca799d21d3cdefe6b45b415
Kandrashin Denis <mail@lintest.ru>
2020-06-27 16:47:16 +0300

    Виджеты: оптимизация кода

--- src/widgets/subcode.ts
+++ src/widgets/subcode.ts
@@ -5,39 +5,20 @@ export class SubcodeWidget implements monaco.editor.IViewZone {
   public heightInLines: number;
   public marginDomNode: HTMLElement;
   public afterColumn: number = 1;
-  public onDomNodeTop(top: number): void {
-    this.top = top;
-  }
 
   public id: number;
-  public top: number;
   private textNode: HTMLElement;
   private leftNode: HTMLElement;
-  private lineHeight: string;
 
   constructor(content: string) {
-    let style = (document.querySelector('div.view-lines') as HTMLElement).style;
-    this.domNode = document.createElement('div');
-    this.domNode.classList.add('vanessa-code-widget');
-    this.lineHeight = style.lineHeight;
-
-    this.leftNode = document.createElement('div');
-    this.leftNode.classList.add('vanessa-code-border');
-    this.domNode.appendChild(this.leftNode);
-
-    this.textNode = document.createElement('div');
-    this.textNode.classList.add('vanessa-code-lines');
-    this.domNode.appendChild(this.textNode);
-
+    this.domNode = this.div('vanessa-code-widget');
+    this.textNode = this.div('vanessa-code-lines', this.domNode);
+    this.leftNode = this.div('vanessa-code-border', this.domNode);
     this.marginDomNode = document.createElement('div');
-
     this.heightInLines = content.split(/\r\n|\r|\n/).length;
     for (let i = 0; i < this.heightInLines; i++) {
-      var glyphNode = document.createElement('div');
-      this.leftNode.appendChild(glyphNode);
-      glyphNode.style.height = this.lineHeight;
+      this.div("", this.leftNode);
     }
-
     monaco.editor.colorize(content, "turbo-gherkin", {})
       .then((html: string) => this.textNode.innerHTML = html);
   }
@@ -50,5 +31,12 @@ export class SubcodeWidget implements monaco.editor.IViewZone {
     this.domNode.dataset.id = String(this.id);
     this.leftNode.dataset.id = String(this.id);
     return this.id;
+  }
+
+  private div(className: string, parent: HTMLElement = null): HTMLElement {
+    let node = document.createElement('div');
+    if (className) node.classList.add(className);
+    if (parent) parent.appendChild(node);
+    return node;
   }
 }
