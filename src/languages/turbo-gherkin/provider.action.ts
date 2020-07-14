abd9b5cfa43c61ae759eaa3c8a9ecc8bd24a34ed
Kandrashin Denis <mail@lintest.ru>
2020-07-14 19:32:24 +0300

    Quick Fix: параметры и ключевое слово #41 и #93

--- src/languages/turbo-gherkin/provider.action.ts
+++ src/languages/turbo-gherkin/provider.action.ts
@@ -7,42 +7,57 @@ export class ActionProvider extends ProviderBase {
     let range = {
       startLineNumber: error.startLineNumber,
       endLineNumber: error.endLineNumber,
-      startColumn: error.startColumn,
+      startColumn: 1,
       endColumn: error.endColumn,
     };
     let value = model.getValueInRange(range);
-    let words = this.key(this.filterWords(this.splitWords(value))).split(' ');
+    let words = this.splitWords(value);
+    let keyword = this.findKeyword(words);
+    if (keyword == undefined) return;
+    let regexp = "^[\\s]+";
+    keyword.forEach(w => regexp += w + "[\\s]+");
+    let match = value.toLowerCase().match(new RegExp(regexp));
+    if (match) range.startColumn = match[0].length + 1;
+    let line = this.key(this.filterWords(words)).split("-");
     for (let key in this.steps) {
       let sum = 0; let k = {};
-      var step = key.split(' ');
-      words.forEach((w: string) => k[w] ? k[w] += 1 : k[w] = 1);
+      var step = key.split('-');
+      line.forEach((w: string) => k[w] ? k[w] += 1 : k[w] = 1);
       step.forEach((w: string) => k[w] ? k[w] -= 1 : k[w] = -1);
       for (let i in k) sum = sum + Math.abs(k[i]);
-      if (sum < 3) list.push({ key: key, sum: sum, error: error });
+      if (sum < 3) list.push({ key: key, sum: sum, error: error, range: range, words: words });
     }
   }
 
-  // FIX ERROR !!! https://github.com/microsoft/monaco-editor/issues/1548
+  private static replaceParams(step: string[], line: string[]): string {
+    let index = 0;
+    step = this.filterWords(step);
+    let regexp = /^"[^"]*"$|^'[^']*'$|^<[^<]*>$/g;
+    let test = (w: string) => (new RegExp(regexp.source)).test(w);
+    let params = line.filter(w => test(w));
+    return step.map(w => (test(w) && index < params.length) ? params[index++] : w ).join(' ');
+  }
+
   private static getQuickFix(
     model: monaco.editor.ITextModel,
     markers: monaco.editor.IMarkerData[]
   ): monaco.languages.CodeActionList {
     let list = [];
     let actions: Array<monaco.languages.CodeAction> = [];
-    let editor: monaco.editor.IStandaloneCodeEditor = window["VanessaEditor"].editor;
-    markers.forEach(e => this.addQuickFix(editor.getModel(), list, e));
+    markers.forEach(e => this.addQuickFix(model, list, e));
     list.sort((a, b) => a.sum - b.sum);
     list.forEach((e, i) => {
       if (i > 6) return;
       let step = this.steps[e.key];
+      let text = this.replaceParams(step.head, e.words);
       actions.push({
-        title: step.label,
+        title: text,
         diagnostics: [e.error],
         kind: "quickfix",
         edit: {
           edits: [{
             resource: model.uri,
-            edit: { range: e.error, text: step.insertText }
+            edit: { range: e.range, text: text }
           }]
         },
         isPreferred: true
