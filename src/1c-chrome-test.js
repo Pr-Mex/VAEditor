0d4b6f9fe7c99ece2eb7f12c7eb60f2ced33d9b1
Kandrashin Denis <mail@lintest.ru>
2020-07-18 23:42:44 +0300

    Заплатка для события открытия внешней ссылки #96

--- src/1c-chrome-test.js
+++ src/1c-chrome-test.js
@@ -6,6 +6,8 @@ window.onload = () => {
 \tДано Я запускаю сценарий открытия TestClient или подключаю уже существующий\n\
 \tОткрывается тест-клиент\n\nСтруктура сценария:\n\t* Открытие формы элемента\n\
 \t\tЕсли Версия платформы ">=" "8.3.6" Тогда \n\
+\t\tИ я показываю подсказку "ТекстПодсказки" EnjoyHint у элемента "ИмяЭлемента"\n\
+\t\t|"shape"|"rect"|\n\t|"timeout"|"5000"\n\
 \t\tИ видеовставка картинки "$ИмяКнопки$" \'$ИмяРеквизита$\' \n\
 \t\tИ это значит что в таблице "$ИмяТаблицы$" есть колонка с именем \'$ИмяРеквизита$\' Тогда\n\
 \t\tЕсли в панели открытых есть команда "ЗаписатьИЗакрыть" Тогда\n\
@@ -33,6 +35,14 @@ window.onload = () => {
         "documentation": "",
         "kind": 0,
         "section": "Прочее.Версия платформы"
+    },
+    {
+        "filterText": "я показываю подсказку \"ТекстПодсказки\" EnjoyHint у элемента \"ИмяЭлемента\"\n\u0009\u0009|'selector'|'button'|\n\u0009\u0009|'showSkip'|'false'|\n\u0009\u0009|'shape'|'rect'|\n\u0009\u0009|'timeout'|'5000'|",
+        "insertText": "И я показываю подсказку \"ТекстПодсказки\" EnjoyHint у элемента \"ИмяЭлемента\"\n\u0009\u0009|'selector'|'button'|\n\u0009\u0009|'showSkip'|'false'|\n\u0009\u0009|'shape'|'rect'|\n\u0009\u0009|'timeout'|'5000'|",
+        "sortText": "И я показываю подсказку \"ТекстПодсказки\" EnjoyHint у элемента \"ИмяЭлемента\"\n\u0009\u0009|'selector'|'button'|\n\u0009\u0009|'showSkip'|'false'|\n\u0009\u0009|'shape'|'rect'|\n\u0009\u0009|'timeout'|'5000'|",
+        "documentation": "Рисует фигуры в барузере. Параметры описаны тут: https://github.com/xbsoftware/enjoyhint/blob/master/README.md",
+        "kind": 4,
+        "section": "Прочее.Браузер"
     },
     {
         "filterText": "в окне предупреждения нет текста \"Нужный текст\" тогда",
@@ -141,30 +151,31 @@ window.onload = () => {
 
     let subcode = '\tК тому же шаг подсценария 1\n\t\tИ шаг подсценария 2\n\t\t\tИ шаг подсценария 3\n\t\t\tИ шаг подсценария 4\n\t\
     И шаг подсценария 5\n\t\t\tИ шаг подсценария 6\n\t\tИ шаг подсценария 7\n\t\t\tИ шаг подсценария 8\n\tИ шаг подсценария 9';
+    VanessaEditor.showRuntimeCode(20, subcode);
     VanessaEditor.showRuntimeCode(28, subcode);
-//    VanessaEditor.showRuntimeCode(20, subcode + "\n");
-/*
-    setInterval(() => VanessaEditor.runtimeManager.next(), 500);
-    let problems = [{
-        "lineNumber": 12,
-        "severity": "Warning",
-        "message": "Runtime error",
-        "code": "0x1005",
-        "source": "Data info",
-    }];
-    VanessaEditor.decorateProblems(JSON.stringify(problems));
-*/
-    let error = "Runtime error info";
-    VanessaEditor.setRuntimeProgress("complete", 15);
-    VanessaEditor.setRuntimeProgress("error", 17);
-    VanessaEditor.setRuntimeProgress("disabled", 19);
-    VanessaEditor.setRuntimeProgress("pending", 24);
-    VanessaEditor.setCurrentProgress(21);
-    VanessaEditor.showRuntimeError(17, 0, "0x01", error);
-    VanessaEditor.setTheme("vs-dark");
+    /*
+        setInterval(() => VanessaEditor.runtimeManager.next(), 500);
+        let problems = [{
+            "lineNumber": 12,
+            "severity": "Warning",
+            "message": "Runtime error",
+            "code": "0x1005",
+            "source": "Data info",
+        }];
+        VanessaEditor.decorateProblems(JSON.stringify(problems));
 
-    setTimeout(() => {
-        VanessaEditor.setRuntimeProgress("error", 3, "b1");
-        VanessaEditor.showRuntimeError(3, "b1", "0x01", error);
-    }, 1000);
+        let error = "Runtime error info";
+        VanessaEditor.setRuntimeProgress("complete", 15);
+        VanessaEditor.setRuntimeProgress("error", 17);
+        VanessaEditor.setRuntimeProgress("disabled", 19);
+        VanessaEditor.setRuntimeProgress("pending", 24);
+        VanessaEditor.setCurrentProgress(21);
+        VanessaEditor.showRuntimeError(17, 0, "0x01", error);
+        VanessaEditor.setTheme("vs-dark");
+
+        setTimeout(() => {
+            VanessaEditor.setRuntimeProgress("error", 3, "b1");
+            VanessaEditor.showRuntimeError(3, "b1", "0x01", error);
+        }, 1000);
+    */
 }
