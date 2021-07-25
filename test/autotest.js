62af3d59a80196e80871a68d90a894805db5efd1
Kandrashin Denis <mail@lintest.ru>
2021-07-25 11:24:50 +0300

    Тесты для AppVeyor

--- test/autotest.js
+++ test/autotest.js
@@ -5,11 +5,18 @@ import * as dom from 'monaco-editor/esm/vs/base/browser/dom'
 
 const $ = dom.$
 
-const send = (url, data) => {
-  var xhr = new XMLHttpRequest();
-  xhr.open("POST", url, true);
-  xhr.setRequestHeader("Content-Type", "application/json");
-  xhr.send(JSON.stringify(data));
+const send = (url, test, err, status) => {
+  var xhr = new XMLHttpRequest()
+  xhr.open("POST", url, true)
+  xhr.setRequestHeader("Content-Type", "application/json")
+  xhr.send(JSON.stringify({
+    outcome: status,
+    testName: test.title,
+    fileName: test.parent.title,
+    ErrorMessage: err.message,
+    ErrorStackTrace: err.stack,
+    durationMilliseconds: test.duration,
+  }))
 }
 
 const autotest = (url) => {
@@ -38,61 +45,19 @@ const autotest = (url) => {
   const context = require.context('.', true, /.+\.ts$/)
   context.keys().forEach(context)
   var runner = mocha.run()
-  var failedTests = []
 
-  if (url) runner.on('test', (test) =>
-    send(url + "api/tests", {
-      outcome: "Running",
-      testName: test.title,
-      fileName: test.parent.title,
-      durationMilliseconds: 0,
-    })
-  );
-
-  if (url) runner.on('pass', (test) =>
-    send(url + "api/tests", {
-      outcome: "Passed",
-      testName: test.title,
-      fileName: test.parent.title,
-      durationMilliseconds: test.duration,
-    })
-  );
-
-  if (url) runner.on('fail', (test) =>
-    send(url + "api/tests", {
-      outcome: "Failed",
-      testName: test.title,
-      fileName: test.parent.title,
-      ErrorMessage: err.message,
-      durationMilliseconds: test.duration,
-    })
-  );
+  if (url) {
+    runner.on('test', (test, err) => send(url + "api/tests", test, err, "Running"))
+    runner.on('pass', (test, err) => send(url + "api/tests", test, err, "Passed"))
+    runner.on('fail', (test, err) => send(url + "api/tests", test, err, "Failed"))
+  }
 
   runner.on('end', () => {
     window.mochaResults = runner.stats
-    window.mochaResults.reports = failedTests
     document.getElementById('VanessaContainer').classList.add('vanessa-hidden')
-    dom.removeClass(domMain, 'vanessa-hidden')
     const button = $("button.vanessa-hidden", { id: "AutotestResult" })
     document.body.appendChild(button).click()
-  })
-
-  runner.on('fail', (test, err) => {
-    var flattenTitles = function (test) {
-      var titles = []
-      while (test.parent.title) {
-        titles.push(test.parent.title)
-        test = test.parent
-      }
-      return titles.reverse()
-    }
-    failedTests.push({
-      name: test.title,
-      result: false,
-      message: err.message,
-      stack: err.stack,
-      titles: flattenTitles(test)
-    })
+    dom.removeClass(domMain, 'vanessa-hidden')
   })
 
   delete window.VanessaAutotest
