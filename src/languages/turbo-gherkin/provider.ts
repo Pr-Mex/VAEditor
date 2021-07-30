e18ba1c66b9ac8a2fd0916b4c12feace53f2cff7
Kandrashin Denis <mail@lintest.ru>
2021-07-30 16:56:40 +0300

    Сворачивание многострочных параметров

--- src/languages/turbo-gherkin/provider.ts
+++ src/languages/turbo-gherkin/provider.ts
@@ -21,6 +21,7 @@ enum VAToken {
   Section,
   Operator,
   Comment,
+  Multiline,
   Instruction,
   Parameter,
 }
@@ -371,10 +372,17 @@ export class VanessaGherkinProvider {
     lineCount: number,
     getLineContent: (lineNumber: number) => string
   ): Array<monaco.languages.FoldingRange> {
+    let multiline = false;
     let lines: Array<VAIndent> = [{ token: VAToken.Empty, indent: 0 }];
     for (let lineNumber = 1; lineNumber <= lineCount; lineNumber++) {
       let text = getLineContent(lineNumber);
       let token = this.getToken(text);
+      if (multiline) {
+        if (token == VAToken.Multiline) multiline = false;
+        token = VAToken.Multiline;
+      } else {
+        if (token == VAToken.Multiline) multiline = true;
+      }
       if (token != VAToken.Operator) {
         lines.push({ token: token, indent: 0 });
       } else {
@@ -413,6 +421,7 @@ export class VanessaGherkinProvider {
             if (next.token == VAToken.Section) break;
             if (next.token == VAToken.Empty) continue;
             if (next.token == VAToken.Comment) { k = j; continue; }
+            if (next.token == VAToken.Multiline) { k = j; continue; }
             if (next.token == VAToken.Parameter) { k = j; continue; }
             if (next.indent <= line.indent) break; else k = j;
           } break;
