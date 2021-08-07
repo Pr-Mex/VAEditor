14d1601fd459b8f4581c674ad851b6c198bf3fc1
Kandrashin Denis <mail@lintest.ru>
2021-08-07 20:12:30 +0300

    Тест проверки синтаксиса

--- test/syntax/example.file.js
+++ test/syntax/example.file.js
@@ -1,2 +1,2 @@
-import f from './example.feature'
-export const content = String(f)
+import f01 from './example.01.feature'
+export const content01 = String(f01)
--- test/syntax/syntax.test.ts
+++ test/syntax/syntax.test.ts
@@ -1,17 +1,14 @@
 import { VanessaGherkinProvider } from '../../src/languages/turbo-gherkin/provider';
-import { content } from './example.file.js'
+import { content01 } from './example.file.js'
 let expect = require('chai').expect;
 
 describe('Проверка синтаксиса', function () {
-  let result: monaco.editor.IMarker[];
-  before(() => {
-    const provider = VanessaGherkinProvider.instance;
-    const model = monaco.editor.createModel(content, "turbo-gherkin");
-    provider.checkSyntax(model);
-    result = monaco.editor.getModelMarkers({owner: "syntax", resource: model.uri});
-    console.log(result);
-  });
+  const provider = VanessaGherkinProvider.instance;
   it('Ключевые слова в описании фичи', () => {
+    const model = monaco.editor.createModel(content01, "turbo-gherkin");
+    provider.checkSyntax(model);
+    const result = monaco.editor.getModelMarkers({owner: "syntax", resource: model.uri});
     expect(result).to.be.an('array').to.have.lengthOf(1);
+    expect(model.getValueInRange(result[0])).to.equal("Когда есть шаг с ошибкой ситнаксиса");
   });
 })
