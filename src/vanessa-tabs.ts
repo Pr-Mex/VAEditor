52619c74cfd5cd04b5c5db526c4ba96b3990f41a
Kandrashin Denis <mail@lintest.ru>
2021-09-16 01:20:50 +0300

    Назначение опций редактора для всех вкладок

--- src/vanessa-tabs.ts
+++ src/vanessa-tabs.ts
@@ -529,4 +529,5 @@ export class VanessaTabs {
   public set theme(arg: string) { StyleManager.theme = arg; }
   public get version(): string { return version }
   public get options(): string { return JSON.stringify(monaco.editor.EditorOptions); }
+  public set options(value: string) { this.tabStack.forEach(tab => tab.options = value); }
 }
